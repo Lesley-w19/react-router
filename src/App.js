a33b7618503927fecebfca3914ece1833782bbfc
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-07 13:56:55 +0300

    Imported Routes, Route from reacr-router-dom Specified the path and element in the route tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 // import logo from "./logo.svg";
 import "./App.css";
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 const App = () => {
   return (
     <div className="App">
-      <h1>react router</h1>
+      {/* <h1>react router</h1> */}
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="about" element={<About/>} />
+        <Route path="contact" element={<Contact/>} />
+      </Routes>
     </div>
   );
 };
