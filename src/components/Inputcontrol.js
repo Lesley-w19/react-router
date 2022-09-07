3a96bbffc0e1d1e9d2b8bb5f47719c369c287fbd
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-07 22:04:47 +0300

    Created an InputControl component that is used to handle the getting of different countries fronm the json file, selection and search of a given country and passing state as props to the AutpComplete compponent

---
+++ src/components/Inputcontrol.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import _ from "lodash";
+// import { Form } from "react-bootstrap";
+import AutoComplete from "./AutoComplete";
+import useOutsideClick from "../custom-hooks/useOClick";
+
+const InputControl = ({ name, label, placeholder }) => {
+  const [countryRef, isVisible, setIsVisible] = useOutsideClick();
+  const [allCountries, setallCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [searchValue, setsearchValue] = useState("");
+  const [error, setError] = useState("");
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = _.debounce(getCountries, 300);
+  }, []);
+
+  function getCountries(inputSearch) {
+    axios
+      .get("/countries.json")
+      .then((response) => {
+        const countries = response.data;
+        const result = countries.filter((country) =>
+          //   console.log(country)
+          country.toLowerCase().includes(inputSearch.toLowerCase())
+        );
+        setallCountries(result);
+        if (result.length > 0) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+        setError("");
+      })
+      .catch(() => setError("Something went wrong. Try again later"));
+  }
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const { value } = event.target;
+    setsearchValue(value);
+    ref.current(value);
+  };
+
+  const handleSuggestionClick = (countryValue) => {
+    setSelectedCountry(countryValue);
+    setIsVisible(false);
+  };
+
+  return (
+    <div controlId="searchValue">
+      <label>{label}</label>
+      <div
+        className="input-control"
+        type="text"
+        value={searchValue}
+        name={name}
+        onChange={handleSearch}
+        autoComplete="off"
+        placeholder={placeholder}
+      />
+      <div ref={countryRef}>
+        {isVisible && (
+          <AutoComplete
+            isVisible={isVisible}
+            suggestions={allCountries}
+            handleSuggestionClick={handleSuggestionClick}
+          />
+        )}
+      </div>
+      {selectedCountry && (
+        <div className="selected-country">
+          Your selected country: {selectedCountry}
+        </div>
+      )}
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+};
+
+export default InputControl;
