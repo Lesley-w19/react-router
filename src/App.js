29bd762ed1c74dbbbe8fc5cd4a333a6a0fa3d5ba
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-07 14:07:12 +0300

    Created routes to the home, about and contact pages Imported  Navbar component and added it to the compoenent

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div className="App">
       {/* <h1>react router</h1> */}
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="about" element={<About/>} />
-        <Route path="contact" element={<Contact/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
