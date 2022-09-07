80d5beb67fd65d580758f69f8cceabe241b5d43e
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-07 13:13:48 +0300

    Created the home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+
+const Home = () =>{
+    return(
+        <Fragment>
+            <h1>Home page</h1>
+            <p> This is the Home page</p>
+        </Fragment>
+    )
+}
+export default Home
