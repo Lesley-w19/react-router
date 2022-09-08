c851d3b8340cc2f76775c9ecbccf594143495dae
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-08 09:27:27 +0300

    Added a wrapper div for styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,11 +4,14 @@ import Button from "../components/Button";
 const About = () => {
   return (
     <>
+    <div className="wrapper">
       <h1>About page</h1>
       <p> This is the About page</p>
 
       <Button variant="info" size={"sm"} color="white">Small button</Button>
       <Button variant="success" size={"lg"} >Smaller Button</Button>
+    </div>
+
     </>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,25 +8,26 @@ const InputReftUse = React.lazy(() => import("../components/InputRefUse"));
 const Contact = () => {
   return (
     <React.Fragment>
-      <h1>Contact page</h1>
-      <p> This is the Contact page</p>
-      <div className="main">
-        <h1>React AutoSuggestion Demo</h1>
-        <div className="search-form">
-          <div>
-            <Input
-              name="country"
-              label="Enter name of a Country"
-              placeholder="Type a country name"
-            />
+      <div className="wrapper contact">
+        <h1>Contact page</h1>
+
+        <div className="main">
+          <div className="search-form">
+            <div>
+              <Input
+                name="country"
+                label="Enter name of a Country"
+                placeholder="Type a country name"
+              />
+            </div>
           </div>
+
+          <Population />
+
+          <Suspense fallback={<Loading />}>
+            <InputReftUse />
+          </Suspense>
         </div>
-
-        <Population />
-
-        <Suspense fallback={<Loading />}>
-          <InputReftUse />
-        </Suspense>
       </div>
     </React.Fragment>
   );
